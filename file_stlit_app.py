f7d35ab383450af837f5ba8ceca1d24068b866c5
Harsha606 <136447394+Harsha606@users.noreply.github.com>
2023-11-01 14:31:55 +0530

    Update file_stlit_app.py

--- file_stlit_app.py
+++ file_stlit_app.py
@@ -41,4 +41,4 @@ if selected_opt=='Sender':
     if uploaded_file is not None:
         # displaying the contents of the CSV file
         dataframe = pd.read_csv(uploaded_file,encoding='unicode_escape')
-        st.write(dataframe)
+        st.write("Sucessfully Loaded File")
