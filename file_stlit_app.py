f7dea079b26cc6f41306adf7f4da6601d559a86f
Harsha606 <136447394+Harsha606@users.noreply.github.com>
2023-11-01 17:37:00 +0530

    Update file_stlit_app.py

--- file_stlit_app.py
+++ file_stlit_app.py
@@ -45,7 +45,7 @@ if selected_opt=='Sender':
         dataframe = pd.read_csv(uploaded_file,nrows=10)
         #data=uploaded_file.read()
         #st.write(data)
-        st.dataframe(dataframe,reindex=True)
+        st.dataframe(dataframe,hide_index=True,use_column_width=True)
   with col2:
     btn1=st.button("Encrypt and Upload File",key="Key1")
     st.write("")
